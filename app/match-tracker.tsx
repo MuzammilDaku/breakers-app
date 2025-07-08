d22f59ff08a45adab47c8d8b7b12086c98c4e45a
MuzammilDaku <khanbalochmuzammilabbas@gmail.com>
2025-07-08 20:29:55 +0500

    Latest

--- app/match-tracker.tsx
+++ app/match-tracker.tsx
@@ -1,40 +1,50 @@
-import { useState } from "react";
+import { useAppStore } from "@/context/appStore";
+import { useLocalSearchParams } from "expo-router";
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-export default function Assign() {
-    const [matchInfo, setMatchInfo] = useState({
-        player_name: '',
-        matchType: '1 v 1',
-        price_frame: ''
-    })
+export default function MatchTracker() {
+    const inUseTables = useAppStore((state)=>state.inUseTables);
+    const tables = useAppStore((state)=>state.tables);
+    const params = useLocalSearchParams();
+    const {table_id} = params;
+    const inUseTable = inUseTables.filter((item)=> item.table._id === table_id)[0];
+    const table = tables.filter((item)=>item._id === table_id)[0];
+    const players : {
+        player_name1:string,
+        player_name2?:string,
+        player_name3?:string,
+        player_name4?:string,
+    }= {
+        player_name1:inUseTable.player_name1
+    };
+    if(inUseTable.game_mode == "2 v 2" && inUseTable.friendly_match) {
+        players.player_name2=inUseTable.player_name2
+    }
+      if(inUseTable.game_mode == "1 v 1" && !inUseTable.friendly_match) {
+        players.player_name2=inUseTable.player_name2
+    }
+     if(inUseTable.game_mode == "2 v 2" && !inUseTable.friendly_match) {
+        players.player_name2=inUseTable.player_name2
+        players.player_name3=inUseTable.player_name3
+        players.player_name4=inUseTable.player_name4
+
+    }
     return (
         <View style={styles.conatiner}>
             <View style={{ marginHorizontal: 25 }}>
-                <View style={styles.inputContainer}>
-                    <Text style={styles.inputLabel}>Player Name *</Text>
+               <View style={{display:"flex",flexDirection:"row",width:"100%"}}>
+                 <View style={styles.inputContainer}>
+                    <Text style={styles.inputLabel}>Winner *</Text>
                     <TextInput placeholder="Enter Player Name" style={styles.input} />
                 </View>
-                <View style={{ marginTop: 15 }}>
-                    <Text style={styles.inputLabel}>Mode</Text>
-
-                    <View style={styles.tabContainer}>
-                        <TouchableOpacity style={[styles.pill, matchInfo.matchType === '1 v 1' && styles.pillSelected]} onPress={() => setMatchInfo({ ...matchInfo, matchType: '1 v 1' })}>
-                            <Text style={styles.pillText}>1 v 1</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={[styles.pill, matchInfo.matchType === '2 v 2' && styles.pillSelected]} onPress={() => { setMatchInfo({ ...matchInfo, matchType: '2 v 2' }) }}>
-                            <Text style={styles.pillText}>2 v 2</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={[styles.pill, matchInfo.matchType === 'Friendly Match' && styles.pillSelected]} onPress={() => { setMatchInfo({ ...matchInfo, matchType: 'Friendly Match' }) }}>
-                            <Text style={styles.pillText}>Friendly Match</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
                 <View style={styles.inputContainer}>
-                    <Text style={styles.inputLabel}>Price Per Frame *</Text>
+                    <Text style={styles.inputLabel}>Loser *</Text>
                     <TextInput placeholder="Enter Player Name" style={styles.input} />
                 </View>
+               </View>
+            
                 <View style={styles.btn}>
                     <TouchableOpacity>
-                        <Text style={{ textAlign: "center", color: "#fefefe", fontSize: 16 }}>Start Match</Text>
+                        <Text style={{ textAlign: "center", color: "#fefefe", fontSize: 16 }}>End Game & Print Bill</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -50,7 +60,10 @@ const styles = StyleSheet.create({
         height: "100%"
     },
     inputContainer: {
-        marginTop: 13
+        marginTop: 13,
+        flex:1,
+        marginRight:5,
+        height:"100%",
     },
     inputLabel: {
         fontSize: 16,
@@ -59,12 +72,13 @@ const styles = StyleSheet.create({
     input: {
         height: 40,
         backgroundColor: "#fefefe",
-        // marginVertical: 10,
         borderRadius: 7,
         paddingHorizontal: 10,
         fontSize: 14,
         borderColor: "#eeeff3",
-        borderWidth: 1
+        borderWidth: 1,
+    
+
     },
     tabContainer: {
         display: 'flex',
