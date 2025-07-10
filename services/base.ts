f399944e3ae1a99ab449eafa9ea90a67d18f8816
MuzammilDaku <khanbalochmuzammilabbas@gmail.com>
2025-07-10 09:19:47 +0500

    Fix Design & Make Builds

--- services/base.ts
+++ services/base.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = "https://db8258ce638e.ngrok-free.app/api"; 
+export const baseUrl = "https://breakersapi.netlify.app/api"; 
 
 const Api = axios.create({
     baseURL: baseUrl, 
